ee13d90420fc4aa01d9f8b5c3c216f51ab88532c
René Cienfuegos <rene@tractical.com>
2012-12-06 17:28:27 -0600

    added AWS connection

--- spec/resources_app_spec.rb
+++ spec/resources_app_spec.rb
@@ -1,4 +1,5 @@
 require './spec/spec_helper'
+require 'debugger'
 
 describe "ResourcesApp" do
 
@@ -6,4 +7,26 @@ describe "ResourcesApp" do
     Sinatra::Application
   end
 
+  subject do
+    Fog.mock!
+    storage = Fog::Storage::AWS.new(aws_access_key_id: 'abcdef', aws_secret_access_key: 'secret')
+  end
+
+  describe '/' do
+    it 'connects to S3' do
+      get '/'
+      subject.should be
+    end
+  end
+
+  describe '/buckets' do
+    before do
+      subject.put_bucket 'resource.bucket'
+    end
+
+    it 'returns a list of buckets' do
+      get '/buckets'
+      subject.directories.should_not be_empty
+    end
+  end
 end

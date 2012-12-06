983f56a9a8776c661751f41a2d87dd883ceb2e2a
René Cienfuegos <rene@tractical.com>
2012-12-06 17:28:27 -0600

    added AWS connection

--- resources_app.rb
+++ resources_app.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'debugger'
+require 'fog'
 
 require 'yaml'
 
 aws_yml = File.expand_path('../aws.yml',  __FILE__)
-AWS_CONFIG = YAML.load_file(aws_yml)
+APP_CONFIG = YAML.load_file(aws_yml)["defaults"]
+
+before do
+  @storage = Fog::Storage::AWS.new(
+    aws_access_key_id: APP_CONFIG["aws_access_key"],
+    aws_secret_access_key: APP_CONFIG["aws_secret_key"])
+end
+
+get '/' do
+  "Connected to AWS" if @storage
+end
+
+get '/buckets' do
+  @directories = @storage.directories.to_s
+end

a7e41ba57d83ee0f09c9e119725b00b3cb272462
René Cienfuegos <rene@tractical.com>
2012-12-19 13:39:43 -0600

    get directories and files from fog

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ require 'rspec'
 require 'rack/test'
 
 set :environment, :test
+set :run, false
+set :raise_errors, true
+
+def app
+  Sinatra::Application
+end
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods

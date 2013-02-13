958a2f2df85e1659c0095df52def5445128c37ce
René Cienfuegos <rene@tractical.com>
2013-02-12 19:48:22 -0600

    added directory creation

--- controllers/resources_controller.rb
+++ controllers/resources_controller.rb
@@ -7,7 +7,7 @@ class ResourcesController < ApplicationController
     if authorized?
       aws_access_key = ENV['S3_KEY']    || settings.aws["access_key"]
       aws_secret_key = ENV['S3_SECRET'] || settings.aws["secret_key"]
-      @storage = Fog::Storage::AWS.new(
+      @storage ||= Fog::Storage::AWS.new(
         aws_access_key_id: aws_access_key,
         aws_secret_access_key: aws_secret_key)
     end
@@ -32,7 +32,6 @@ class ResourcesController < ApplicationController
       splitted_key = file.key.split('/')
       if splitted_key.size >= 2
         parent   = @@tree.find { |node| node.name == splitted_key[-2] }
-        # position = file.key.end_with?('/') && parent != @@tree.root ? 0 : -1
         parent.add(Tree::TreeNode.new(splitted_key.last, file))
       else
         @@tree.add(Tree::TreeNode.new(splitted_key.last, file))
@@ -45,16 +44,20 @@ class ResourcesController < ApplicationController
     erb :'resources/buckets/show'
   end
 
+  # directories#edit
   get '/bucket/:bucket_id/*/edit' do
     bucket = @storage.directories.get(params[:bucket_id])
-    if @file  = bucket.files.get(params[:splat].first + "/")
+    if @file = bucket.files.get(params[:splat].first + "/")
       erb :'resources/directories/edit'
     else
       erb :not_found
     end
   end
 
-  # Update Folder name
+  # directories#update
+  # 1. Copy file with new parameteres first.
+  # 2. Copy children files into new copied file (directory).
+  # 3. Destroy all directory and children files.
   put '/bucket/:bucket_id/*/?' do
     bucket = @storage.directories.get(params[:bucket_id])
     splat = params[:splat].first.gsub(/\/$/, '') + "/"
@@ -84,7 +87,7 @@ class ResourcesController < ApplicationController
     redirect "/resources/bucket/#{bucket.key}/"
   end
 
-  # Delete folder.
+  # directories#destroy
   # Delete files inside the folder first, the folder last.
   delete '/bucket/:bucket_id/*/?' do
     bucket = @storage.directories.get(params[:bucket_id])
@@ -103,6 +106,29 @@ class ResourcesController < ApplicationController
     redirect "/resources/bucket/#{bucket.key}/"
   end
 
+  get '/bucket/:bucket_id/*/directories/new' do
+    @path = [params[:bucket_id], params[:splat].first].join('/') + "/"
+    erb :'resources/directories/new'
+  end
+
+  # directories#create
+  # Create a directory at the given path
+  # Note: Need to create a blank temporary file inside so that the resource
+  # is treated as a directory.
+  post '/bucket/:bucket_id/*/directories/?' do
+    bucket = @storage.directories.get(params[:bucket_id])
+    path = params[:splat].first + "/"
+    name = path + params[:name].gsub(/\//, '') + '/'
+
+    if file = bucket.files.create(key: name)
+      bucket.files.create(key: file.key + "delete me")
+      redirect "resources/bucket/#{params[:bucket_id]}/"
+    else
+      halt 500
+    end
+  end
+
+  # files#index
   get '/bucket/:bucket_id/*/?' do
     path = params[:splat].first.split('/')
     node = @@tree.find { |node| node.name == path.last }

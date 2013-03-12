9552f6ad402bd79ac417276647526bbed2facefe
René Cienfuegos <rene@tractical.com>
2013-03-12 11:17:04 -0600

    updated foundation and compass config; removed unneeded .css files

--- config/compass.rb
+++ config/compass.rb
@@ -20,6 +20,7 @@ preferred_syntax = :sass
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
